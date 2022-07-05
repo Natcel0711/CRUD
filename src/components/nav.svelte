28acf0072c66d4cc02795463eb4dc42f24ca0d41
natcel <natcelnieves@gmail.com>
2022-07-04 23:33:52 -0400

    Changing Nav look

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,4 +1,4 @@
-<nav class="flex justify-center w-full border-black border-b">
-  <a class="mx-4  text-xl " href="/">Home</a>
-  <a class="mx-4 text-xl" href="/about">About</a>
+<nav class="flex h-16 justify-center w-full border-black border-b">
+  <a style="line-height: 3.50rem;" class="mx-4 text-xl " href="/">Home</a>
+  <a style="line-height: 3.50rem;" class="mx-4 text-xl" href="/about">About</a>
 </nav>
